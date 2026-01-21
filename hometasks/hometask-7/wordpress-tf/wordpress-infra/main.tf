881d5ae5919e14bc5539c5b295fe45f0db86ae1e
Sun <orionvantix@gmail.com>
2026-01-21 01:03:18 -0600

    fixing

--- hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
+++ hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
@@ -11,37 +11,38 @@ data "aws_subnets" "default" {
 }
 
 module "sg" {
-  source = "git::ssh://git@github.com/orionvantix/terraform.git//hometasks/hometask-7/tf-modules/modules/sg?ref=main"
-
+  source      = "git::ssh://git@github.com/orionvantix/terraform.git//hometasks/hometask-7/tf-modules/modules/sg?ref=main"
   name        = "wordpress-sg"
-  description = "This is Wordpress security group"
+  description = "This is wordpress security group"
   vpc_id      = data.aws_vpc.default.id
 }
 
+
 module "ec2" {
-  source = "git::ssh://git@github.com/orionvantix/terraform.git//tf-modules/modules/ec2?ref=main"
+  source = "git::ssh://git@github.com/orionvantix/terraform.git//hometasks/hometask-7/tf-modules/modules/ec2?ref=main"
 
-  env                  = "wordpress"
-  ami_id               = "ami-07ff62358bb7c7116"
-  instance_type        = "t3.micro"
-  vpc_security_group_ids = [ module.sg.vpc_security_group_ids ]
-  subnet_id            = data.aws_subnets.default.ids[0]
+  # variables
+  env                 = "wordpress"
+  ami_id              = "ami-07ff62358bb7c7116"
+  instance_type       = "t3.micro"
+  vpc_security_group_ids = module.sg.vpc_security_group_ids
+  subnet_id           = [data.aws_subnets.default.ids[0]]
 }
-
 
 
 module "rds" {
-  source = "git::ssh://git@github.com/orionvantix/terraform.git//tf-modules/modules/rds?ref=main"
+  source = "git::ssh://git@github.com/orionvantix/terraform.git//hometasks/hometask-7/tf-modules/modules/rds?ref=main"
 
-  identifier            = "wordpress-db"
-  instance_class        = "db.t3.micro"
-  allocated_storage     = 20
-  username              = "wpuser"
-  password              = "sun123"
-  db_name               = "wordpress"
+  identifier          = "wordpress-db"
+  instance_class      = "db.t3.micro"
+  allocated_storage   = 20
+  username            = "wpuser"
+  password            = "sun123"
+  db_name             = "wordpress"
 
   vpc_security_group_id = module.sg.vpc_security_group_ids[0]
 }
+
 
 
 
