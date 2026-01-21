6199a6ad3bb9f77807591b34dc2cb242f5d9b3a9
Sun <orionvantix@gmail.com>
2026-01-20 23:58:20 -0600

    another upgrade

--- hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
+++ hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
@@ -11,34 +11,34 @@ data "aws_subnets" "default" {
 }
 
 module "sg" {
-  # call module
-  source      = "../../tf-modules/modules/sg"
+  source      = "git@github.com:orionvantix/tf-modules.git//modules/sg"
   name        = "wordpress-sg"
   description = "This is Wordpress security group"
   vpc_id      = data.aws_vpc.default.id
 }
 
 module "ec2" {
-  source = "../../tf-modules/modules/ec2"
-  # variables
+  source = "git@github.com:orionvantix/tf-modules.git//modules/ec2"
+
   env                    = "wordpress"
-  ami                    = "ami-07ff62358b87c7116"
+  ami                    = "ami-07ff62358bb7c7116"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [module.sg.vpc_security_group_ids]
   subnet_id              = data.aws_subnets.default.ids[0]
 }
 
 module "rds" {
-  source = "../../tf-modules/modules/rds"
+  source = "git@github.com:orionvantix/tf-modules.git//modules/rds"
 
-  identifier         = "wordpress-db"
-  instance_class     = "db.t3.micro"
-  allocated_storage  = 20
-  username           = "wpuser"
-  password           = "sun123"
-  db_name            = "wordpress"
-  vpc_security_group = module.sg.vpc_security_group.ids
+  identifier            = "wordpress-db"
+  instance_class        = "db.t3.micro"
+  allocated_storage     = 20
+  username              = "wpuser"
+  password              = "suni123"
+  db_name               = "wordpress"
+  vpc_security_group_id = module.sg.vpc_security_group_ids
 }
+
 
 # module "s3-bucket" {
 #   source = "https://github.com/orionvantix/terraform//hometasks/hometask-3/modules/s3"
