2e27f789cc30b02f0db5e8066adcb17f64e3439c
Sun <orionvantix@gmail.com>
2026-01-21 01:09:46 -0600

    fixing

--- hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
+++ hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
@@ -1,4 +1,3 @@
-# data to get default vpc
 data "aws_vpc" "default" {
   default = true
 }
@@ -11,47 +10,29 @@ data "aws_subnets" "default" {
 }
 
 module "sg" {
-  source      = "git::ssh://git@github.com/orionvantix/terraform.git//hometasks/hometask-7/tf-modules/modules/sg?ref=main"
+  source      = "git::ssh://git@github.com/orionvantix/terraform.git/hometasks/hometask-7/tf-modules/modules/sg?ref=main"
   name        = "wordpress-sg"
   description = "This is wordpress security group"
   vpc_id      = data.aws_vpc.default.id
 }
 
-
 module "ec2" {
-  source = "git::ssh://git@github.com/orionvantix/terraform.git//hometasks/hometask-7/tf-modules/modules/ec2?ref=main"
-
-  # variables
-  env                 = "wordpress"
-  ami_id              = "ami-07ff62358bb7c7116"
-  instance_type       = "t3.micro"
+  source                = "git::ssh://git@github.com/orionvantix/terraform.git/hometasks/hometask-7/tf-modules/modules/ec2?ref=main"
+  env                   = "wordpress"
+  ami_id                = "ami-07ff623588bc7f116"
+  instance_type         = "t3.micro"
   vpc_security_group_ids = module.sg.vpc_security_group_ids
-  subnet_id           = [data.aws_subnets.default.ids[0]]
+  subnet_id             = data.aws_subnets.default.ids[0]
 }
-
 
 module "rds" {
-  source = "git::ssh://git@github.com/orionvantix/terraform.git//hometasks/hometask-7/tf-modules/modules/rds?ref=main"
-
-  identifier        = "wordpress-db"
-  instance_class    = "db.t3.micro"
-  allocated_storage = 20
-  username          = "wpuser"
-  password          = "sun123"
-  db_name           = "wordpress"
-
+  source             = "git::ssh://git@github.com/orionvantix/terraform.git/hometasks/hometask-7/tf-modules/modules/rds?ref=main"
+  identifier         = "wordpress-db"
+  instance_class     = "db.t3.micro"
+  allocated_storage  = 20
+  username           = "wpuser"
+  password           = "sun123"
+  db_name            = "wordpress"
   vpc_security_group_id = module.sg.vpc_security_group_ids[0]
-
-  subnet_ids = data.aws_subnets.default.ids
+  subnet_ids         = data.aws_subnets.default.ids
 }
-
-
-
-
-
-# module "s3-bucket" {
-#   source = "https://github.com/orionvantix/terraform//hometasks/hometask-3/modules/s3"
-# #   version = "value"
-
-#   bucket = "terraform-hw-3-bucket"
-# }
