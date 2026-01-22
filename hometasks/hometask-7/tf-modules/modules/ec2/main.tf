52ac6e68a72154d10de026215d8572c1123caac6
Sun <orionvantix@gmail.com>
2026-01-22 13:43:13 -0600

    fixing

--- hometasks/hometask-7/tf-modules/modules/ec2/main.tf
+++ hometasks/hometask-7/tf-modules/modules/ec2/main.tf
@@ -4,6 +4,11 @@ resource "aws_instance" "main" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
   user_data              = var.user_data
+    iam_instance_profile   = var.iam_instance_profile
+
+
+    key_name               = "MyMacKey"
+
 
     associate_public_ip_address = true
 
