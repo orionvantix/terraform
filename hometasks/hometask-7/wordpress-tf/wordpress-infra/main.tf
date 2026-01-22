1e35c639d1bb5c7a4a35e3e37c132a634c2259cf
Sun <orionvantix@gmail.com>
2026-01-22 15:49:20 -0600

    fixing

--- hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
+++ hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
@@ -1,6 +1,3 @@
-data "aws_vpc" "default" {
-  default = true
-}
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
@@ -19,29 +16,12 @@ data "aws_ami" "amazon_linux" {
     values = ["hvm"]
   }
 }
-# resource "aws_subnet" "db_a" {
-#   vpc_id                  = data.aws_vpc.default.id
-#   cidr_block              = "172.31.101.0/24"
-#   availability_zone       = "us-east-1a"
-#   map_public_ip_on_launch = false
-#   tags = {
-#     Name = "wordpress-db-a"
-#   }
-# }
-# resource "aws_subnet" "db_b" {
-#   vpc_id                  = data.aws_vpc.default.id
-#   cidr_block              = "172.31.102.0/24"
-#   availability_zone       = "us-east-1b"
-#   map_public_ip_on_launch = false
-#   tags = {
-#     Name = "wordpress-db-b"
-#   }
-# }
+
 module "sg" {
   source      = "git::https://github.com/orionvantix/terraform.git//hometasks/hometask-7/tf-modules/modules/sg?ref=main"
   name        = "wordpress-sg"
   description = "This is wordpress security group"
-  
+
   vpc_id = aws_vpc.wp.id
 }
 module "ec2" {
@@ -50,7 +30,7 @@ module "ec2" {
   env                    = var.env
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t3.micro"
-  subnet_id              = data.aws_subnets.default.ids[0]
+  subnet_id              = aws_subnet.public_a.id
   vpc_security_group_ids = module.sg.ids
   user_data              = file("${path.module}/wp_userdata.sh")
 }
