08b932cf713588a824c914eb374d222e7baa105b
Sun <orionvantix@gmail.com>
2026-01-22 18:47:21 -0600

    fixing

--- hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
+++ hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
@@ -7,7 +7,7 @@ module "sg" {
 
   http_cidrs  = ["0.0.0.0/0"]
   ssh_cidrs   = ["0.0.0.0/0"]
-  mysql_cidrs = ["0.0.0.0/0"]
+  mysql_cidrs = ["10.0.0.0/16"]
 }
 
 module "ec2" {
@@ -17,7 +17,7 @@ module "ec2" {
   env                    = var.env
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public_a.id
-  vpc_security_group_ids = [module.sg.id]
+  vpc_security_group_ids = module.sg.ids
   user_data = templatefile("${path.module}/wp_userdata.sh.tpl", {
     db_name     = var.db_name
     db_username = var.db_username
