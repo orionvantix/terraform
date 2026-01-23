d0ee5d9158558b57e8d3ef58061027b769375f9d
Sun <orionvantix@gmail.com>
2026-01-22 19:12:37 -0600

    fixing

--- hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
+++ hometasks/hometask-7/wordpress-tf/wordpress-infra/main.tf
@@ -13,19 +13,19 @@ module "sg" {
 module "ec2" {
   source = "git::https://github.com/orionvantix/terraform.git//hometasks/hometask-7/tf-modules/modules/ec2?ref=main"
 
-  name  = "wordpress-ec2"
-  env   = var.env
+  name = "wordpress-ec2"
+  env  = var.env
 
-  instance_type         = "t3.micro"
-  subnet_id             = aws_subnet.public_a.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public_a.id
   vpc_security_group_ids = module.sg.ids
 
   user_data = templatefile("${path.module}/wp_userdata.sh.tpl", {
-    db_name     = var.db_name
-    db_username = var.db_username
-    db_password = var.db_password
-    db_host     = module.rds.endpoint
-  })
+  db_name     = var.db_name
+  db_user     = var.db_username  # <- change this key name
+  db_password = var.db_password
+  db_host     = module.rds.endpoint
+})
 
   key_name            = "wp-key"
   associate_public_ip = true
